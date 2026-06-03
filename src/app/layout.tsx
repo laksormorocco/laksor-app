9658848803df631196337f8d76f5185c214b1667
laksormorocco <laksor.morocco@gmail.com>
2026-06-03 13:50:51 +0000

    Revert "fix: rename globals.css"
    This reverts commit 5308a1831245ff605aaaf14bde989fc5e2ae90a0.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
-import "./globals-styles.css";
+import "./globals.css";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
