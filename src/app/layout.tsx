34a4a0867bd5aa0319985175dad3651db4a72fda
laksormorocco <laksor.morocco@gmail.com>
2026-06-03 14:01:08 +0000

    fix: layout import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
-import "./globals.css";
+import "./global-styles.css";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
